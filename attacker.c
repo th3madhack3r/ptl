b6d05ffef3e6bd5e22d8c931459b05b0c70454aa
th3madhack3r <wsponsl@wgu.edu>
2024-01-08 03:13:37 +0100

    attack

--- attacker.c
+++ attacker.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-static void maliciois() __attribute__((constructior));
+static void malicious() __attribute__((constructor));
 
 void malicious() {
     system("/usr/local/bin/score 2256c7b9-d70f-4ed6-a612-7e46a37d7beb");
